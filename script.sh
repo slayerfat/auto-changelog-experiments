01062072c6f09857f6364efa5dcf3aef183a49d0
Alejandro Granadillo <slayerfat@gmail.com>
2017-05-01 12:25:13 -0400

    fix(cli): fixes npm version Git working directory not clean error

--- script.sh
+++ script.sh
@@ -4,6 +4,9 @@ BUMP=`node_modules/conventional-recommended-bump/cli.js -p angular`
 
 echo The recommended bump is ${BUMP}
 
+echo making package.json copy
+cp package.json original.package.json
+
 echo using npm version with no tag
 npm --no-git-tag-version version ${BUMP} &>/dev/null
 VERSION=`cat package.json | node_modules/json/lib/json.js version`
@@ -12,8 +15,11 @@ echo altering the CHANGELOG.md
 node_modules/conventional-changelog-cli/cli.js -i CHANGELOG.md -s -p angular
 
 echo commiting
-git add CHANGELOG.md
+git add CHANGELOG.md package
 git commit -m "docs: bump to $VERSION"
+
+echo restoring package.json
+mv -f original.package.json package.json
 
 echo npm version
 npm version ${BUMP} -m "chore: $VERSION release"
