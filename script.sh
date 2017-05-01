586e1142eee24aa0e05c87904d9d6ba82dde305a
Alejandro Granadillo <slayerfat@gmail.com>
2017-05-01 13:28:02 -0400

    fix(script): remove npm run command from the script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BUMP=`npm run recommended-bump`
+BUMP=`node_modules/conventional-recommended-bump/cli.js -p angular`
 
 echo The recommended bump is ${BUMP}
 
@@ -9,23 +9,23 @@ cp package.json original.package.json
 
 echo using npm version with no tag
 npm --no-git-tag-version version ${BUMP} &>/dev/null
-VERSION=`npm run json`
+VERSION=`cat package.json | node_modules/json/lib/json.js version`
 
 echo altering the CHANGELOG.md
-npm run changelog-cli
+node_modules/conventional-changelog-cli/cli.js -i CHANGELOG.md -s -p angular
 
 echo commiting
 git add CHANGELOG.md
-git commit -m "docs: bump to $VERSION"
+git commit -m "docs(changelog): bump to $VERSION"
 
 echo restoring package.json
 mv -f original.package.json package.json
 
 echo npm version
-npm version ${BUMP} -m "chore: $VERSION release"
+npm version ${BUMP} -m "chore(release): $VERSION release"
 
 echo pushing
 git push --follow-tags
 
 echo altering github release metadata
-npm run recommended-bump
+node_modules/conventional-github-releaser/cli.js -p angular
